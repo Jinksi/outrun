740155bc8d0cbf92660d1f45fecb9781744105c7
Eric Jinks <ericjinks@gmail.com>
2016-08-15 14:28:15 +1000

    Remove play-sound dep / add PlaySound.stop()

--- outrun.js
+++ outrun.js
@@ -1,24 +1,63 @@
-const player = require('play-sound')(opts = {})
-const spawn = require('child_process').spawn
+const child_process = require('child_process')
+const spawn = child_process.spawn
 const clc = require('cli-color')
+const fs = require('fs')
+const findExec = require('find-exec')
 
 
-var setupThrobber = require('cli-color/throbber');
+function PlaySound(){
+  this.players = [
+    'mplayer',
+    'afplay',
+    'mpg123',
+    'mpg321',
+    'play',
+    'omxplayer'
+  ]
 
-var throbber = setupThrobber(function (str) {
-  var text = '.'
-  var style = { ".": clc.xterm(13)("⚡︎") }
-  process.stdout.write(clc.art(text, style))
-}, 100)
+  this.player = findExec(this.players)
 
-throbber.start()
+  this.play = function(sound){
+    try {
+      isFile = fs.statSync(sound).isFile()
+    } catch (err){
+      isFile = false
+    }
 
-// at any time you can stop/start throbber
-// throbber.stop()
+    if (!sound) return next(new Error("No audio file specified"));
+
+    if (!isFile){
+      return new Error(sound + " is not a file")
+    }
+
+    if (!this.player){
+      return new Error("Couldn't find a suitable audio player")
+    }
+
+    this.child = child_process.execFile(this.player, [sound], function(err, stdout, stderr){
+      // handle error
+    })
+  }
+
+  this.stop = function(){
+    if(this.child) this.child.kill()
+  }
+}
+
+const player = new PlaySound()
 
 player.play('outrun.m4a', function(err){
   console.log(err)
 })
+
+const setupThrobber = require('cli-color/throbber');
+
+const throbber = setupThrobber(function (str) {
+  const text = '.'
+  const style = { ".": clc.xterm(13)("⚡︎") }
+  process.stdout.write(clc.art(text, style))
+}, 100)
+throbber.start()
 
 const cmd = process.argv[2]
 let args = process.argv
@@ -27,12 +66,14 @@ args.splice(0, 3)
 if(cmd){
   const task = spawn(cmd, args)
   task.stdout.on('data', (data) => {
-    console.log(`${data}`)
+    console.log(`\n${data}`)
   })
   task.stderr.on('data', (data) => {
-    console.log(`${data}`)
+    console.log(`\n${data}`)
   })
   task.on('close', (code) => {
-    
+    player.stop()
+    throbber.stop()
+    process.exit()
   })
 }
