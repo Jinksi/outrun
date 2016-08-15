c9e88d5531f8a8192b536a3f9124d6e7765be318
Eric Jinks <ericjinks@gmail.com>
2016-08-15 14:49:48 +1000

    Fix global path to audio

--- outrun.js
+++ outrun.js
@@ -4,6 +4,7 @@ const child_process = require('child_process')
 const spawn = child_process.spawn
 const clc = require('cli-color')
 const fs = require('fs')
+const path = require('path')
 const findExec = require('find-exec')
 
 function PlaySound(){
@@ -18,25 +19,25 @@ function PlaySound(){
 
   this.player = findExec(this.players)
 
-  this.play = function(sound){
+  this.play = function(sound, callback){
     try {
       isFile = fs.statSync(sound).isFile()
     } catch (err){
       isFile = false
     }
 
-    if (!sound) return next(new Error("No audio file specified"));
+    if (!sound) return callback(new Error("No audio file specified"))
 
     if (!isFile){
-      return new Error(sound + " is not a file")
+      return callback(new Error(sound + " is not a file"))
     }
 
     if (!this.player){
-      return new Error("Couldn't find a suitable audio player")
+      return callback(new Error("Couldn't find a suitable audio player"))
     }
 
     this.child = child_process.execFile(this.player, [sound], function(err, stdout, stderr){
-      // handle error
+      callback(err)
     })
   }
 
@@ -47,11 +48,11 @@ function PlaySound(){
 
 const player = new PlaySound()
 
-player.play('outrun.m4a', function(err){
+player.play(__dirname + '/outrun.m4a', function(err){
   console.log(err)
 })
 
-const setupThrobber = require('cli-color/throbber');
+const setupThrobber = require('cli-color/throbber')
 
 const throbber = setupThrobber(function (str) {
   const text = '.'
